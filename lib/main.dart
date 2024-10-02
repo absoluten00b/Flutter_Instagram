b4261fbf8fc4982932339efb926e14f9f84d64c6
absoluten00b <whiteviolent8@gmail.com>
2024-10-01 23:03:32 -0700

    Defined colors in class

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:app/Utils/Colors.dart';
 import 'package:flutter/material.dart';
 
 void main(){
   runApp(MaterialApp(
     title: 'Instagram',
-    theme: ThemeData.dark(),
+    theme: ThemeData.dark().copyWith(
+      scaffoldBackgroundColor: mobileBackgroundColor
+    ),
     home: const Scaffold(body: Text('Let\'s build it')) ,
   ));
 }
