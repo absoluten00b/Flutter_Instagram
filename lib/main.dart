7525a9d2aa242472e34fe58d3ad0ab438bf3e0e1
absoluten00b <whiteviolent8@gmail.com>
2024-10-01 22:49:10 -0700

    Init Instagram Clone With Flutter
    In this project I'll commit step by step instructuction how you can build Instagram clone using Flutter

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: 'Instagram',
+    theme: ThemeData.dark(),
+    home: const Scaffold(body: Text('Let\'s build it')) ,
+  ));
+}
